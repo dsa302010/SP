82de3933b16eb250bc2ef0985c129b43b507d40f
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2025-09-17 19:18:43 +0800

    音量

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -17,8 +17,8 @@ from openpilot.selfdrive.ui.sunnypilot.quiet_mode import QuietMode
 
 SAMPLE_RATE = 48000
 SAMPLE_BUFFER = 4096 # (approx 100ms)
-MAX_VOLUME = 1.0
-MIN_VOLUME = 0.1
+MAX_VOLUME = 0.06
+MIN_VOLUME = 0.06
 SELFDRIVE_STATE_TIMEOUT = 5 # 5 seconds
 FILTER_DT = 1. / (micd.SAMPLE_RATE / micd.FFT_SAMPLES)
 
